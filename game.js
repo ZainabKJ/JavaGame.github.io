4ad016c957af01542eef009be433b617ad366866
ZainabKJ <90893721+ZainabKJ@users.noreply.github.com>
2021-10-22 21:46:02 +0400

    Changes in HTML and Javascript

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ function Bear() {
   this.x = this.htmlElement.offsetLeft;
   this.y = this.htmlElement.offsetTop;
   this.move = function (xDir, yDir) {
+    this.fitBounds();
     this.x += this.dBear * xDir;
     this.y += this.dBear * yDir;
     this.display();
@@ -14,21 +15,31 @@ function Bear() {
     this.htmlElement.style.top = this.y + "px";
     this.htmlElement.style.display = "block";
   };
+  this.fitBounds = function () {
+    let parent = this.htmlElement.parentElement;
+    let iw = this.htmlElement.offsetWidth;
+    let ih = this.htmlElement.offsetHeight;
+    let l = parent.offsetLeft;
+    let t = parent.offsetTop;
+    let w = parent.offsetWidth;
+    let h = parent.offsetHeight;
+    if (this.x < 0) this.x = 0;
+    if (this.x > w - iw) this.x = w - iw;
+    if (this.y < 0) this.y = 0;
+    if (this.y > h - ih) this.y = h - ih;
+  };
 }
-
 function start() {
   //create bear
   bear = new Bear();
-  // Add an event listener to the keypress event.
   document.addEventListener("keydown", moveBear, false);
   //create new array for bees
   bees = new Array();
   //create bees
   makeBees();
+  lastStingTime = new Date();
 }
 
-// Handle keyboad events
-// to move the bear
 function moveBear(e) {
   //codes of the four keys
   const KEYUP = 38;
@@ -48,26 +59,6 @@ function moveBear(e) {
     bear.move(0, 1);
   } // down key
 }
-this.fitBounds = function () {
-  let parent = this.htmlElement.parentElement;
-  let iw = this.htmlElement.offsetWidth;
-  let ih = this.htmlElement.offsetHeight;
-  let l = parent.offsetLeft;
-  let t = parent.offsetTop;
-  let w = parent.offsetWidth;
-  let h = parent.offsetHeight;
-  if (this.x < 0) this.x = 0;
-  if (this.x > w - iw) this.x = w - iw;
-  if (this.y < 0) this.y = 0;
-  if (this.y > h - ih) this.y = h - ih;
-};
-
-this.move = function (xDir, yDir) {
-  this.fitBounds(); //we add this instruction to keep bear within board
-  this.x += this.dBear * xDir;
-  this.y += this.dBear * yDir;
-  this.display();
-};
 
 class Bee {
   constructor(beeNumber) {
@@ -108,6 +99,7 @@ class Bee {
     };
   }
 }
+
 function createBeeImg(wNum) {
   //get dimension and position of board div
   let boardDiv = document.getElementById("board");
@@ -134,6 +126,10 @@ function createBeeImg(wNum) {
   return img;
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function makeBees() {
   //get number of bees specified by the user
   let nbBees = document.getElementById("nbBees").value;
@@ -153,7 +149,6 @@ function makeBees() {
     i++;
   }
 }
-
 function moveBees() {
   //get speed input field value
   let speed = document.getElementById("speedBees").value;
@@ -165,7 +160,6 @@ function moveBees() {
     isHit(bees[i], bear); //we add this to count stings
   }
 }
-
 function updateBees() {
   // update loop for game
   //move the bees randomly
@@ -194,35 +188,27 @@ function isHit(defender, offender) {
     }
     document.getElementById("duration").innerHTML = longestDuration;
   }
+}
 
-  function overlap(element1, element2) {
-    //consider the two rectangles wrapping the two elements
-    //rectangle of the first element
-    left1 = element1.htmlElement.offsetLeft;
-    top1 = element1.htmlElement.offsetTop;
-    right1 = element1.htmlElement.offsetLeft + element1.htmlElement.offsetWidth;
-    bottom1 =
-      element1.htmlElement.offsetTop + element1.htmlElement.offsetHeight;
-    //rectangle of the second element
-    left2 = element2.htmlElement.offsetLeft; //e2x
-    top2 = element2.htmlElement.offsetTop; //e2y
-    right2 = element2.htmlElement.offsetLeft + element2.htmlElement.offsetWidth;
-    bottom2 =
-      element2.htmlElement.offsetTop + element2.htmlElement.offsetHeight;
-    //calculate the intersection of the two rectangles
-    x_intersect = Math.max(
-      0,
-      Math.min(right1, right2) - Math.max(left1, left2)
-    );
-    y_intersect = Math.max(
-      0,
-      Math.min(bottom1, bottom2) - Math.max(top1, top2)
-    );
-    intersectArea = x_intersect * y_intersect;
-    //if intersection is nil no hit
-    if (intersectArea == 0 || isNaN(intersectArea)) {
-      return false;
-    }
-    return true;
+function overlap(element1, element2) {
+  //consider the two rectangles wrapping the two elements
+  //rectangle of the first element
+  left1 = element1.htmlElement.offsetLeft;
+  top1 = element1.htmlElement.offsetTop;
+  right1 = element1.htmlElement.offsetLeft + element1.htmlElement.offsetWidth;
+  bottom1 = element1.htmlElement.offsetTop + element1.htmlElement.offsetHeight;
+  //rectangle of the second element
+  left2 = element2.htmlElement.offsetLeft; //e2x
+  top2 = element2.htmlElement.offsetTop; //e2y
+  right2 = element2.htmlElement.offsetLeft + element2.htmlElement.offsetWidth;
+  bottom2 = element2.htmlElement.offsetTop + element2.htmlElement.offsetHeight;
+  //calculate the intersection of the two rectangles
+  x_intersect = Math.max(0, Math.min(right1, right2) - Math.max(left1, left2));
+  y_intersect = Math.max(0, Math.min(bottom1, bottom2) - Math.max(top1, top2));
+  intersectArea = x_intersect * y_intersect;
+  //if intersection is nil no hit
+  if (intersectArea == 0 || isNaN(intersectArea)) {
+    return false;
   }
+  return true;
 }
